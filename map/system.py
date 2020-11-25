3bf244a0f969e3e16e79a68de236801a53d90acc
Matheus Guerreiro <73855206+matheusguerreiro@users.noreply.github.com>
2020-11-25 17:23:49 -0300

    commit

--- map/system.py
+++ map/system.py
@@ -1,3 +1,29 @@
-from map.module import criarRevista
+def criarRevista():
+    class revista:
+        def __init__(self, colecao, edicao, ano):
+            self.colecao = colecao
+            self.edicao = edicao
+            self.ano = ano
 
-r1 = criarRevista()
+        def setColecao(self, colecao):
+            self.colecao = colecao
+        def getColecao(self):
+            return self.colecao
+
+        def setEdicao(self, edicao):
+            self.edicao = edicao
+        def getEdicao(self):
+            return self.edicao
+
+        def setAno(self, ano):
+            self.ano = ano
+        def getAno(self):
+            return self.ano
+
+    colecao = str(input('Coleção: '))
+    edicao = int(input('Edição: '))
+    ano = int(input('Ano: '))
+
+    r = revista(colecao, edicao, ano)
+    return r
+
